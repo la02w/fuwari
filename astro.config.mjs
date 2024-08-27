ed79fd4c9e846d1b4d731f6d85aca3e7f916fffd
La02 <1786075785@qq.com>
2024-08-27 10:56:17 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: 'https://la02.github.io',
+  // base: '/',
   trailingSlash: "always",
   integrations: [
     tailwind(),
